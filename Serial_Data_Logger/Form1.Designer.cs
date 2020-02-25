355c2e073941228ccb95b9350d100168091e8458
Dinusha <dinusha89.usjp@gmail.com>
2020-02-26 02:02:55 +0530

    First edit

--- Serial_Data_Logger/Form1.Designer.cs
+++ Serial_Data_Logger/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Serial_Data_Logger
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,12 +29,275 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbBAUD = new System.Windows.Forms.ComboBox();
+            this.cmbComPort = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnShiftLeft = new System.Windows.Forms.Button();
+            this.btnShiftRight = new System.Windows.Forms.Button();
+            this.txtShift = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.dataPlot = new ScottPlot.FormsPlot();
+            this.serialPort2 = new System.IO.Ports.SerialPort(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cmbBAUD);
+            this.panel1.Controls.Add(this.cmbComPort);
+            this.panel1.Controls.Add(this.btnConnect);
+            this.panel1.Controls.Add(this.btnShiftLeft);
+            this.panel1.Controls.Add(this.btnShiftRight);
+            this.panel1.Controls.Add(this.txtShift);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtCount);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(139, 411);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "BAUD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Port";
+            // 
+            // cmbBAUD
+            // 
+            this.cmbBAUD.FormattingEnabled = true;
+            this.cmbBAUD.Items.AddRange(new object[] {
+            "110",
+            "300",
+            "600",
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "14400",
+            "19200",
+            "38400",
+            "57600",
+            "115200",
+            "128000",
+            "256000"});
+            this.cmbBAUD.Location = new System.Drawing.Point(53, 31);
+            this.cmbBAUD.Name = "cmbBAUD";
+            this.cmbBAUD.Size = new System.Drawing.Size(79, 21);
+            this.cmbBAUD.TabIndex = 8;
+            this.cmbBAUD.Text = "115200";
+            // 
+            // cmbComPort
+            // 
+            this.cmbComPort.FormattingEnabled = true;
+            this.cmbComPort.Location = new System.Drawing.Point(53, 4);
+            this.cmbComPort.Name = "cmbComPort";
+            this.cmbComPort.Size = new System.Drawing.Size(79, 21);
+            this.cmbComPort.TabIndex = 7;
+            this.cmbComPort.Text = "COM1";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.Red;
+            this.btnConnect.Location = new System.Drawing.Point(15, 58);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(117, 24);
+            this.btnConnect.TabIndex = 6;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnShiftLeft
+            // 
+            this.btnShiftLeft.Enabled = false;
+            this.btnShiftLeft.Location = new System.Drawing.Point(16, 141);
+            this.btnShiftLeft.Name = "btnShiftLeft";
+            this.btnShiftLeft.Size = new System.Drawing.Size(31, 23);
+            this.btnShiftLeft.TabIndex = 5;
+            this.btnShiftLeft.Text = "<<";
+            this.btnShiftLeft.UseVisualStyleBackColor = true;
+            // 
+            // btnShiftRight
+            // 
+            this.btnShiftRight.Enabled = false;
+            this.btnShiftRight.Location = new System.Drawing.Point(105, 141);
+            this.btnShiftRight.Name = "btnShiftRight";
+            this.btnShiftRight.Size = new System.Drawing.Size(27, 23);
+            this.btnShiftRight.TabIndex = 4;
+            this.btnShiftRight.Text = ">>";
+            this.btnShiftRight.UseVisualStyleBackColor = true;
+            // 
+            // txtShift
+            // 
+            this.txtShift.Enabled = false;
+            this.txtShift.Location = new System.Drawing.Point(53, 143);
+            this.txtShift.Name = "txtShift";
+            this.txtShift.Size = new System.Drawing.Size(46, 20);
+            this.txtShift.TabIndex = 3;
+            this.txtShift.Text = "0";
+            this.txtShift.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Count";
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(53, 106);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(79, 20);
+            this.txtCount.TabIndex = 1;
+            this.txtCount.Text = "1044";
+            this.txtCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(15, 170);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(117, 25);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.btnMinimize);
+            this.panel2.Controls.Add(this.btnClose);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(854, 39);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.Location = new System.Drawing.Point(786, 7);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(25, 25);
+            this.btnMinimize.TabIndex = 1;
+            this.btnMinimize.Text = "_";
+            this.btnMinimize.UseVisualStyleBackColor = true;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(817, 7);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(25, 25);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // dataPlot
+            // 
+            this.dataPlot.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataPlot.Location = new System.Drawing.Point(139, 39);
+            this.dataPlot.Name = "dataPlot";
+            this.dataPlot.Size = new System.Drawing.Size(715, 411);
+            this.dataPlot.TabIndex = 2;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 400;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 18);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Serial Data Logger";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(854, 450);
+            this.Controls.Add(this.dataPlot);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "Serial Data Logger";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnMinimize;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnShiftLeft;
+        private System.Windows.Forms.Button btnShiftRight;
+        private System.Windows.Forms.TextBox txtShift;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.ComboBox cmbComPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbBAUD;
+        private ScottPlot.FormsPlot dataPlot;
+        private System.IO.Ports.SerialPort serialPort2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Serial_Data_Logger/Form1.cs
+++ Serial_Data_Logger/Form1.cs
@@ -3,18 +3,173 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Serial_Data_Logger
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        double[] x = new double[1044];
+        byte[] data = new byte[1044];
+        byte[] incommingData = new byte[1044];
+        double[] processedData = new double[1044];
+        Random rnd = new Random();
+        int count = 1044;
+
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void UpdateData()
+        {
+            count = Convert.ToInt32(txtCount.Text);
+            x = new double[count];
+            data = new byte[count];
+            incommingData = new byte[count];
+            processedData = new double[count];
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void ProcessData()
+        {
+            for (int i = 0; i < count; i++)
+                processedData[i] = Convert.ToDouble(incommingData[i]);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            btnClose.Location = new Point(this.Width - 28, btnClose.Location.Y);
+            btnMinimize.Location = new Point(this.Width - 56, btnMinimize.Location.Y);
+            cmbComPort.Items.AddRange(SerialPort.GetPortNames());
+            //serialPort2.BaudRate = 115200;
+            //serialPort2.PortName = "COM2";
+            //serialPort2.StopBits = StopBits.One;
+            //serialPort2.Parity = Parity.None;
+            //serialPort2.DataBits = 8;
+            //serialPort2.Open();
+            //while (!serialPort2.IsOpen) ;
+            //MessageBox.Show("Connected!");
+            //for (int i = 0; i < 1044; i++)
+            //{
+            //    x[i] = i;
+            //    data[i] = Convert.ToByte(rnd.NextDouble() * 3.5 * (1 + Math.Sin((i / 1043.0) * Math.PI * 2)));
+            //    //data[i] = Convert.ToByte(i % 8);
+            //}
+        }
+
+        private void ReGenData()
+        {
+            double k = rnd.NextDouble() * 3.5;
+            for (int i = 0; i < 1044; i++)
+            {
+                x[i] = i;
+                data[i] = Convert.ToByte(k * (1 + Math.Sin((i / 1043.0) * Math.PI * 2)));
+                //data[i] = Convert.ToByte(i % 8);
+            }
+        }
+
+
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            //ReGenData();
+            //serialPort2.Write(data, 0, data.Length);
+            //Thread.Sleep(500);
+            UpdateData();
+        }
+
+        private void UpdateGraph()
+        {
+            ProcessData();
+
+            dataPlot.plt.Clear();
+            Color figureBgColor = ColorTranslator.FromHtml("#001021");
+            Color dataBgColor = ColorTranslator.FromHtml("#021d38");
+            dataPlot.plt.Style(figBg: figureBgColor, dataBg: dataBgColor);
+            dataPlot.plt.Grid(color: ColorTranslator.FromHtml("#273c51"));
+            dataPlot.plt.Ticks(color: Color.LightGray, useMultiplierNotation: false);
+            dataPlot.plt.PlotScatter(x, processedData, label: "Series 1");
+            dataPlot.plt.Title("");
+            dataPlot.plt.XLabel("Pixel Number", color: Color.White);
+            dataPlot.plt.YLabel("ADC value", color: Color.White);
+            dataPlot.plt.AxisAuto();
+            //dataPlot.plt.Legend(true, fontColor: Color.White);
+            dataPlot.Render();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Text == "Connect")
+            {
+                serialPort1.BaudRate = Convert.ToInt32(cmbBAUD.Text);
+                serialPort1.PortName = cmbComPort.Text;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Parity = Parity.None;
+                serialPort1.DataBits = 8;                                            // attempt to connect to USB board
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(DataReceivedEvent);
+                serialPort1.Open();
+                while (!serialPort1.IsOpen) ;
+                //MessageBox.Show("Connected!");
+                timer1.Start();
+                //backgroundWorker1.RunWorkerAsync();
+                btnConnect.Text = "Disconnect";
+                btnConnect.BackColor = Color.LimeGreen;
+            }
+            else
+            {
+                serialPort1.Close();
+                while (serialPort1.IsOpen) ;
+
+                btnConnect.Text = "Connect";
+                btnConnect.BackColor = Color.Red;
+            }
+        }
+
+        private void DataReceivedEvent(object sender, SerialDataReceivedEventArgs e)
+        {
+            
+            //int MSB,LSB,val = 0;
+            //int j = 0;
+
+            serialPort1.Read(incommingData, 0, count);
+
+            //while(serialPort1.BytesToRead != 0)
+            //{
+            //    MSB = serialPort1.ReadByte();
+            //    //LSB = serialPort1.ReadByte();
+            //    //val = (Convert.ToByte(MSB) << 2) | (Convert.ToByte(LSB) >> 6);
+            //    incommingData[j] = Convert.ToDouble(MSB);
+            //    j++;
+            //    if (j > count - 1)
+            //        j = 0;
+            //    //Console.WriteLine("Received" + serialPort1.BytesToRead);
+            //}
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //serialPort2.Write(data, 0, data.Length);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateGraph();
+            //Console.WriteLine("updated!");
         }
     }
 }
